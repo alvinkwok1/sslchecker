c63c3e2f6dbfcfe62f27680a68b093e731f58eb3
alvinkwok <1305167657@qq.com>
2024-04-23 00:12:45 +0800

    11

--- sslchecker-server/src/main/java/cn/alvinkwok/sslchecker/WebSiteCertificates.java
+++ sslchecker-server/src/main/java/cn/alvinkwok/sslchecker/WebSiteCertificates.java
@@ -2,6 +2,7 @@ package cn.alvinkwok.sslchecker;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.security.auth.x500.X500Principal;
 import java.io.IOException;
 import java.net.URL;
 import java.security.cert.Certificate;
@@ -9,7 +10,7 @@ import java.security.cert.X509Certificate;
 
 public class WebSiteCertificates {
     public static void main(String[] args) {
-        String url = "https://blog.alvinkwok.cn"; // 替换为你要获取证书信息的域名
+        String url = "https://www.cloudflare.com"; // 替换为你要获取证书信息的域名
 
         try {
             URL serverUrl = new URL(url);
@@ -18,14 +19,23 @@ public class WebSiteCertificates {
             // 获取服务器证书
             Certificate[] certificates = connection.getServerCertificates();
             for (Certificate certificate : certificates) {
-                System.out.println("Certificate: " + certificate);
+                String type = certificate.getType();
+                System.out.println(type);
+
+//                System.out.println("Certificate: " + certificate);
                 // 计算证书的剩余有效期
                 if (certificate instanceof X509Certificate) {
                     X509Certificate x509Certificate = (X509Certificate) certificate;
-                    System.out.println("Subject DN: " + x509Certificate.getSubjectDN());
-                    System.out.println("Issuer DN: " + x509Certificate.getIssuerDN());
+                    System.out.println("Version: " + x509Certificate.getVersion());
+                    System.out.println("Serial Number: " + x509Certificate.getSerialNumber());
+                    X500Principal subjectPrincipal = x509Certificate.getSubjectX500Principal();
+                    System.out.println("Subject DN: " + subjectPrincipal.getName());
+                    X500Principal issuerPrincipal = x509Certificate.getIssuerX500Principal();
+                    System.out.println("Issuer DN: " + issuerPrincipal.getName());
                     System.out.println("Not Before: " + x509Certificate.getNotBefore());
                     System.out.println("Not After: " + x509Certificate.getNotAfter());
+                    System.out.println("Algorithm: " + x509Certificate.getSigAlgName());
+
                 }
             }
             connection.disconnect();
---
+++ sslchecker-server/src/main/java/cn/alvinkwok/sslchecker/web/SslCheckerController.java
@@ -0,0 +1,10 @@
+package cn.alvinkwok.sslchecker.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SslCheckerController {
+
+
+
+}
