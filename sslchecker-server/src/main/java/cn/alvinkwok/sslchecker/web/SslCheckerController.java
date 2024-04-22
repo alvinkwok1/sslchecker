40d772f4d81106d73477e646cf709640c7d4f462
alvinkwok <1305167657@qq.com>
2024-04-23 00:15:06 +0800

    11

--- sslchecker-server/src/main/java/cn/alvinkwok/sslchecker/web/SslCheckerController.java
+++ sslchecker-server/src/main/java/cn/alvinkwok/sslchecker/web/SslCheckerController.java
@@ -5,6 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SslCheckerController {
 
+    /**
+     * 检查网站证书
+     * @param url
+     * @param port
+     * @return
+     */
+    public String checkWebCert(String url,String port) {
+        return "";
+    }
 
 
 }
